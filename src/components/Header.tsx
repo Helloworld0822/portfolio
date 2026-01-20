262a2024cfab1458d845049da978e1387aa03d6c
전재민 <a100822@naver.com>
2026-01-21 05:10:32 +0900

    Chore: react-dom 설치

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <>
+            <header className="">
+                <a href="#">Home</a>                    
+                <a href="#Story">Story</a> 
+                <a href="#Timeline">Timeline</a>    
+                <a href="#Skills">Skills</a>
+                <a href="#Projects">Projects</a>    
+                <a href="#Contact">Contact</a>    
+            </header>
+
+        </>
+    );
+};
+
+export default Header;
